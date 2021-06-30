6fcee9fd2a959435e4bd64c2041b42ee87e33484
Krishnakali Sarkar <krishnakalisarkar@Krishnakalis-MacBook-Pro.local>
2021-06-29 20:33:40 -0500

    Webpage set up completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,12 @@
 // import the data deom data.js
-const tableData = Data;
+const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 //------------------------------------------------------------
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
+    // Next, loop through each object in the data and append a row and cells for each value in the row
     data.forEach((dataRow) => {
         // Append a row to the table body
         let row = tbody.append("tr");
@@ -25,7 +24,7 @@ function buildTable(data) {
 function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
-    let filterData = tableData;
+    let filteredData = tableData;
     // Check to see if a date was entered and filter the data using that date.
     if (date) {
         // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
